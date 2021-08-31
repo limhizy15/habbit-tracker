c1e6db5c77b4d3b11997606c19f13f0cc3bbac1d
[2기-2반] 임희지 <limhizy15@ajou.ac.kr>
2021-09-01 00:46:47 +0900

    Habits 완성

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,24 +1,25 @@
 import React, { Component } from "react";
 
 class Habit extends Component {
-  state = {
-    count: 0,
-  };
-
+  //  props로 전달받은 콜백함수를 호출하도록 만들 것
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
+    this.props.onIncrement(this.props.habit);
   };
 
   handleDecrement = () => {
-    const count = this.state.count - 1;
-    this.setState({ count: count < 0 ? 0 : count });
+    this.props.onDecrement(this.props.habit);
+  };
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
   };
 
   render() {
+    const { name, count } = this.props.habit;
     return (
       <li className="habit">
-        <span className="habit-name">Reading</span>
-        <span className="habit-count">{this.state.count}</span>
+        <span className="habit-name">{name}</span>
+        <span className="habit-count">{count}</span>
         <button
           className="habit-button habit-increase"
           onClick={this.handleIncrement}
@@ -31,7 +32,10 @@ class Habit extends Component {
         >
           <i className="fas fa-minus"></i>
         </button>
-        <button className="habit-button habit-delete">
+        <button
+          className="habit-button habit-delete"
+          onClick={this.handleDelete}
+        >
           <i className="far fa-trash-alt"></i>
         </button>
       </li>
